e5e4011ee3d4d8fbf08babeb1fae9716d2fea815
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-07-29 12:14:53 +0100

    add validation to new debtor creation

--- src/routes/v1/debtor.js
+++ src/routes/v1/debtor.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { addNew } from '../../controllers/debtor/DebtorController';
+import debtorValidation from '../../middlewares/debtorValidation';
+
+const { validateDebtor } = debtorValidation;
 
 const debtorRouter = express.Router();
 
-debtorRouter.post('/debtor', addNew );
+debtorRouter.post('/debtor', validateDebtor, addNew );
 
 
 export default debtorRouter;
