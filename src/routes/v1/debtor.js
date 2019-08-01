eb7cce72cdd7b36a75bffc1467da0b75324bfb22
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-08-01 11:58:22 +0100

    add feature to get all debtors

--- src/routes/v1/debtor.js
+++ src/routes/v1/debtor.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addNew, updateDebtorInfo } from '../../controllers/debtor/DebtorController';
+import { addNew, updateDebtorInfo, getAllDebtors } from '../../controllers/debtor/DebtorController';
 import { verifyToken } from '../../middlewares/jwtHandler';
 import debtorValidation from '../../middlewares/debtorValidation';
 
@@ -8,7 +8,9 @@ const { validateDebtor, validateDebtorStatus } = debtorValidation;
 const debtorRouter = express.Router();
 
 debtorRouter.post('/debtor', verifyToken, validateDebtor, addNew );
-debtorRouter.put('/debtor', verifyToken, validateDebtorStatus, updateDebtorInfo)
+debtorRouter.put('/debtor', verifyToken, validateDebtorStatus, updateDebtorInfo);
+
+debtorRouter.get('/debtors', verifyToken, getAllDebtors);
 
 
 export default debtorRouter;
