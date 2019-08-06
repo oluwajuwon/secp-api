ccea4cba95ce688f6940b3c88199b416420ff02e
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-08-06 13:15:16 +0100

    add feature for admin to signup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 const {express, bodyParser, cors, helmet, morgan, swaggerUI, swaggerDocument} = require('./base/initialDependencies');
 const { useInitialDependencies } = require('./base/initialDependenciesUse');
 const { startServer } = require('./base/serverStarter');
-import { homeRouter, schoolRouter, debtorRouter } from './routes/v1';
+import { homeRouter, schoolRouter, debtorRouter, adminRouter } from './routes/v1';
 
 dotenv.config();
 
@@ -13,7 +13,8 @@ useInitialDependencies(app, bodyParser, cors, helmet, morgan, swaggerUI, swagger
 startServer(app)
 
 app.use('/api/v1/school', schoolRouter);
-app.use('/api/v1/', debtorRouter);
+app.use('/api/v1', debtorRouter);
+app.use('/api/v1/admin', adminRouter);
 
 app.use('/api/v1', homeRouter);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,6 @@
 import homeRouter from './home';
 import schoolRouter from './school';
 import debtorRouter from './debtor';
+import adminRouter from './admin';
 
-export { homeRouter, schoolRouter, debtorRouter };
+export { homeRouter, schoolRouter, debtorRouter, adminRouter };
